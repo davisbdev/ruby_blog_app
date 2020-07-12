33e23b9b3178dda1bd1ee04b8d2f0ecdd3caafd4
davisbdev <davis.barillas@gmail.com>
2020-07-12 14:15:50 -0700

    first

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  root 'welcome#index'
+end
+
+
+
+
+
+
+
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
